c974ce38e2d76f1814dd0d3b7a542f786dd96324
ShuntaToda <todashuntawork@gmail.com>
2024-04-30 11:11:57 +0900

    index.tsのInvoice型定義

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import { statement } from "./statement";
+import { Invoice } from "./types";
 
 function readJSONSync(filePath: any) {
   return JSON.parse(fs.readFileSync(filePath, { encoding: "utf-8" }));
@@ -8,6 +9,6 @@ function readJSONSync(filePath: any) {
 const invoices = readJSONSync("./invoices.json");
 const plays = readJSONSync("./plays.json");
 
-invoices.forEach((invoice: any) => {
+invoices.forEach((invoice: Invoice) => {
   console.log(statement(invoice, plays));
 });
